9101db2ef1f6ed95d5b1680fb523837eee1f9ad8
Sumaiya Promi <spromi460@gmail.com>
2026-04-22 21:31:18 +0600

    Add files via upload

---
+++ 2D Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int arr[3][4];
+   for(int i=0; i<3; i++){
+    for(int j=0; j<4; j++){
+        printf("A[%d][%d]=",i,j);
+        scanf("%d",&arr[i][j]);
+    }
+   } for(int i=0; i<3; i++){
+   for(int j=0; j<4; j++) {
+    printf("%d ",arr[i][j]);
+   } printf("\n");
+
+   } return 0;
+}
